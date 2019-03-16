7b3213827128b7c41bea574bbd06db41b668afa8
jaoropeza <aoropezauribe@gmail.com>
2019-03-16 12:28:04 -0600

    Inicia proyecto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Biblioteca Albo
+
+Aplicación que consulta la biblioteca de comics [Marvel](https://developer.marvel.com) 
+
+## Desarrollo
+
+
+## Producción
+
+
+## Testing
---
+++ src/main/java/com/jaoropeza/albo/library/AlboLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.jaoropeza.albo.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlboLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlboLibraryApplication.class, args);
+    }
+
+}
