a0fd21fe5c28fa4355d7287dc61a29be1bbd913d
Angel Oropeza <angel.oropeza@imexperts.com.mx>
2019-03-19 22:28:59 -0600

    feat: Sincronización de biblioteca

--- src/main/java/com/jaoropeza/albo/library/service/MarvelCron.java
+++ src/main/java/com/jaoropeza/albo/library/service/MarvelCron.java
@@ -2,6 +2,7 @@ package com.jaoropeza.albo.library.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,20 @@ public class MarvelCron {
 
     private final Logger log = LoggerFactory.getLogger(MarvelCron.class);
 
-    @Scheduled(cron = "${albo.marvel.sync.cron}")
+    private final MarvelService marvelService;
+
+    @Autowired
+    public MarvelCron(MarvelService marvelService) {
+        this.marvelService = marvelService;
+    }
+
+    /**
+     * Tarea programada que mantiene actualizados los registros en BD de la biblioteca
+     */
+    @Scheduled(cron = "0 0 0 * * *")
     public void syncMarvel() {
+        log.info("Synchronization process started");
+        marvelService.syncLibrary();
+        log.info("Synchronization process ended");
     }
 }
